f5e8deaa1811c29a1c54009221b6102500710d6f
Daniel Franz Ferreira Fiedler <daniel.fiedler@mundowap.com.br>
2017-10-09 15:46:41 -0300

    Descrição de Rest Organização de Pastas Instalação e teste do composer

--- ordenadoAResultados/index.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-/*
- *  Projeto de criação de ordenação de Resultados
- *  Usado em diversos sites e sistemas para diversos fins
- *
- *
- */
---
+++ pratica/installComposer/index.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: desenvolvimento
+ * Date: 09/10/2017
+ * Time: 15:34
+ */
+
+echo "Hello World";
