94cab11b3c2c4ceee15618834cec3ee5bbf76a0a
Daniel Franz Ferreira Fiedler <daniel.fiedler@mundowap.com.br>
2017-10-09 16:10:40 -0300

    Fix Index Name of Wordpress

---
+++ pratica/wordpressInstall/teste.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: desenvolvimento
+ * Date: 09/10/2017
+ * Time: 16:10
+ */
+
+echo "fjlsd kfjsdlkfj slçdkfjsdlfj sdfjl sdhfksdf sdjfçsdjfskldhf kjsdg bvdfjkbg kldgd";
